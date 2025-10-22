4a8c785a03789b0351052fc766f76b4bd3465c33
Arthur Kozhemyakin <trable228@icloud.com>
2025-10-22 14:53:58 +0300

    refactor
    Co-authored-by: Marmeladkavv <mariavakulina34597@gmail.com>

--- journal/models.py
+++ journal/models.py
@@ -1,33 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
-class BaseTask(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s')
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    date = models.DateField()
+    is_weekly = models.BooleanField(default=False)  # Флаг для определения недельной задачи
 
     def toggle_done(self):
         self.is_done = not self.is_done
         self.save()
 
     class Meta:
-        abstract = True
-        ordering = ['created_at']
-
-
-class DailyTask(BaseTask):
-    date = models.DateField()
-
-    class Meta(BaseTask.Meta):
         ordering = ['date', 'created_at']
-
-
-class WeeklyTask(BaseTask):
-    week_start = models.DateField()
-
-    class Meta(BaseTask.Meta):
-        ordering = ['week_start', 'created_at']
